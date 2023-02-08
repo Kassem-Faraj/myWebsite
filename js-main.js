1718cbd99ce462c255f5451788d08e247922fd6a
Kassem <kassem.faraj99@gmail.com>
2023-02-08 19:47:30 +0200

    Commit before push

--- js-main.js
+++ js-main.js
@@ -1,12 +1,33 @@
+//Responsive NavBar
 let toggleOpen = document.getElementById('open');
 let toggleClose = document.getElementById('close');
 let menu = document.getElementById('navbar-container');
 
-toggleOpen.addEventListener('click', function() {
+toggleOpen.addEventListener('click', () => {
     menu.classList.add('menu-visible');
 });
 
-toggleClose.addEventListener('click', function(){
+toggleClose.addEventListener('click', () =>{
     menu.classList.remove('menu-visible');
 });
+
+//toTop button(show/hide)
+let toTop = document.getElementById('toTop');
+
+window.onscroll = ()=>{
+    if(window.pageYOffset >=50){
+        toTop.style.display = "block";
+        toTop.style.opacity = "3";
+    }else {
+        toTop.style.display = "none";
+    }
+}
+//toTop button(scroll to top)
+toTop.onclick = () => {
+    window.scrollTo({
+        top: 0,
+        behavior:'smooth'   
+    })
+}
+
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   //Look for all HTML files in build dir
-  content: ['/'],
+  content: ['*.html'],
   theme: {
     extend: {},
   },
